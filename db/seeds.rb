262b618351093ebe95b4113f12c9e23982ca682f
Michael Holroyd <meekohi@gmail.com>
2013-01-11 09:48:40 -0500

    update for rails security bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+Group.create([
+  {:fullname => "Blue Ridge Mountain Rescue Group", :shortname => "BRMRG", :location => "Charlottesville, VA"},
+  {:fullname => "Appalachian Search and Rescue Conference", :shortname => "ASRC", :location => "Charlottesville, VA"},
+  {:fullname => "Shenandoah Mountain Rescue Group", :shortname => "SMRG", :location => "Vienna, VA"},  
+  {:fullname => "Delmarva Search and Rescue", :shortname => "DMVSAR", :location => "Salisbury, MD"},
+  {:fullname => "Allegheny Mountain Rescue Group", :shortname => "AMRG", :location => "Pittsburg, PA"},
+  {:fullname => "Maryland Search and Rescue", :shortname => "MSAR", :location => "Columbia, MD"},
+  {:fullname => "Mountaineer Area Rescue Group", :shortname => "MARG", :location => "Kingwood, WV"}
+  ])
